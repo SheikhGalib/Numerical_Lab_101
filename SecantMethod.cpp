6f0f6e816b08da47648c438ee7c58f4f54c46996
SheikhGalib <galibmahim01@gmail.com>
2024-10-26 19:34:26 +0600

    updated some commments

--- SecantMethod.cpp
+++ SecantMethod.cpp
@@ -1,26 +1,25 @@
 #include "SecantMethod.h"
+using namespace std;
 
 double SecantMethod::evaluateEquation(double x) {
-    // Evaluates the equation ax^2 + bx + c
+    // equation type ax^2 + bx + c
     return coefA * x * x + coefB * x + coefC;
 }
 
 void SecantMethod::secantMethod() {
-    // Get user input for coefficients
-    std::cout << "Enter the coefficients for the quadratic equation (ax^2 + bx + c = 0):\n";
-    std::cout << "a: "; std::cin >> coefA;
-    std::cout << "b: "; std::cin >> coefB;
-    std::cout << "c: "; std::cin >> coefC;
 
-    // Get user input for initial guesses, tolerance, and maximum iterations
-    std::cout << "Enter initial guesses x0 and x1:\n";
-    std::cout << "x0: "; std::cin >> x0;
-    std::cout << "x1: "; std::cin >> x1;
+     cout << "Enter the coefficients for the quadratic equation (ax^2 + bx + c = 0):\n";
+     cout << "a: ";  cin >> coefA;
+     cout << "b: ";  cin >> coefB;
+     cout << "c: ";  cin >> coefC;
 
-    std::cout << "Enter tolerance: "; std::cin >> tolerance;
-    std::cout << "Enter maximum iterations: "; std::cin >> maxIterations;
+     cout << "Enter initial guesses x0 and x1:\n";
+     cout << "x0: ";  cin >> x0;
+     cout << "x1: ";  cin >> x1;
 
-    // Start finding the root
+     cout << "Enter tolerance: ";  cin >> tolerance;
+     cout << "Enter maximum iterations: ";  cin >> maxIterations;
+
     findRoot();
 }
 
@@ -29,20 +28,20 @@ void SecantMethod::findRoot() {
     int iteration = 0;
 
     while (iteration < maxIterations) {
-        // Calculate next point x2 using the Secant Method formula
+        // Secant Method formula
         x2 = x1 - evaluateEquation(x1) * ((x1 - x0) / (evaluateEquation(x1) - evaluateEquation(x0)));
 
-        // Check for convergence
-        if (std::abs(evaluateEquation(x2)) <= tolerance) {
-            std::cout << "Root found at: " << x2 << " after " << iteration + 1 << " iterations.\n";
+        // Check for convergence as no exact root
+        if ( abs(evaluateEquation(x2)) <= tolerance) {
+             cout << "Root found at: " << x2 << " after " << iteration + 1 << " iterations.\n";
             return;
         }
 
-        // Update guesses for the next iteration
+        // Update guesses
         x0 = x1;
         x1 = x2;
         iteration++;
     }
 
-    std::cout << "Root approximation after max iterations: " << x2 << "\n";
+     cout << "Root approximation after max iterations: " << x2 << "\n";
 }
